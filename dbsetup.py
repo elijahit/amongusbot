bf6695f3bbb2560a55159b2de748fb8b4b80c2f5
Nicola Occelli <nicola@Nicolas-MBP.lan>
2020-09-28 11:42:33 +0200

    script to generate db schema

---
+++ dbsetup.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+create_users_table = """ CREATE TABLE IF NOT EXISTS users (
+                                    channel_id integer,
+                                    user_id integer
+                                ); """
+
+create_tickets_table = """CREATE TABLE IF NOT EXISTS tickets (
+                                id integer PRIMARY KEY,
+                                name text,
+                                creato text,
+                                joined text
+                                
+                            );"""
+
+if __name__ == '__main__':
+
+    database = 'cogs/prova.db'
+    connection = sqlite3.connect(database)
+    cursor = connection.cursor()
+
+    cursor.execute(create_users_table)
+    cursor.execute(create_tickets_table)
+
